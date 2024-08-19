42640ea3b9c3c2e6b3556ead0fffecdc8e38cad7
Jeffery Abbott <jeff@jefferyabbott.com>
2024-08-18 20:19:58 -0400

    Add calculator

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,20 @@
-function calculator() {}
+function calculator(n1, n2, operand) {
+    switch (operand) {
+        case '+':
+            return n1 + n2;
+        case '-':
+            return n1 - n2;
+        case '*':
+            return n1 * n2;
+        case '/':
+            if (n2 !== 0) {
+                return n1 / n2;
+            } else {
+                throw new Error('Division by zero');
+            }
+        default:
+            throw new Error('Invalid operator');
+    }
+}
 
 module.exports = calculator;
