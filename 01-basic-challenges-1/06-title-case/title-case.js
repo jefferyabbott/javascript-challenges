1dbff739e153d254eeca89692dfe44af2b864117
Jeffery Abbott <jeff@jefferyabbott.com>
2024-08-18 20:37:03 -0400

    Add title case

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,9 @@
-function titleCase() {}
+function titleCase(str) {
+    const words = str.split(' ');
+    for (let i = 0; i < words.length; i++) {
+        words[i] = words[i][0].toUpperCase() + words[i].slice(1).toLowerCase();
+    }
+    return words.join(' ');
+}
 
 module.exports = titleCase;
