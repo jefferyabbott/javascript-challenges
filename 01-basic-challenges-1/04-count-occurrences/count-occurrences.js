9f28e421f66eb6be413fb289588f3cad25c44de4
Jeffery Abbott <jeff@jefferyabbott.com>
2024-08-18 20:26:31 -0400

    Add count occurrences

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,5 @@
-function countOccurrences() {}
+function countOccurrences(str, chr) {
+    return Array.from(str).reduce((sum, letter) => letter === chr ? sum + 1 : sum, 0);
+}
 
 module.exports = countOccurrences;
