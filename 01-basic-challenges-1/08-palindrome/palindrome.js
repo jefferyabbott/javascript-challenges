a6e7a84226b98cdfc80f2532017663714d0f1bbc
Jeffery Abbott <jeff@jefferyabbott.com>
2024-08-20 19:10:46 -0400

    Add palindrome

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,7 @@
-function isPalindrome() {}
+function isPalindrome(str) {
+    const regex = /[^a-zA-Z0-9]/g
+    str = str.replace(regex, '').toLowerCase();
+    return str === str.split('').reverse().join('');
+}
 
 module.exports = isPalindrome;
