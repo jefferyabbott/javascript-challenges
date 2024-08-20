d2cb09304849b245f39320744be4400ecfab5360
Jeffery Abbott <jeff@jefferyabbott.com>
2024-08-20 18:53:19 -0400

    Add reverse string

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,10 @@
-function reverseString() {}
+function reverseString(str) {
+    const strArr = str.split('');
+    let reversedStr = '';
+    while (strArr.length > 0) {
+        reversedStr += strArr.pop();
+    }
+    return reversedStr;
+}
 
 module.exports = reverseString;
