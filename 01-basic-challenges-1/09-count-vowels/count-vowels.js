6ee8a205a6430ef3f9f7c09d27473c4b77d712d2
Jeffery Abbott <jeff@jefferyabbott.com>
2024-08-21 06:30:08 -0400

    Add count vowels

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,8 @@
-function countVowels() {}
+function countVowels(str) {
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    return Array.from(str).reduce((vowelCount, letter) => vowels.includes(letter.toLowerCase()) ? vowelCount + 1 : vowelCount, 0);
+}
 
 module.exports = countVowels;
+
+
