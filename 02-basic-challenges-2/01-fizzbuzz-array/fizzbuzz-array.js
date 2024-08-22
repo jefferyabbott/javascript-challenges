e18b4436babd45d70cc9686bdc62ee0124cf611f
Jeffery Abbott <jeff@jefferyabbott.com>
2024-08-22 08:56:45 -0400

    Add FizzBuzz

--- 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
+++ 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
@@ -1,3 +1,16 @@
-function fizzBuzzArray() {}
+function fizzBuzzArray(n) {
+    arr = [];
+    for (let i = 1; i <= n; i++) {
+        text = '';
+        if (i % 3 == 0) {
+            text += 'Fizz'
+        }
+        if (i % 5 == 0) {
+            text += 'Buzz'
+        }
+        arr.push(text === '' ? i : text);
+    }
+    return arr;
+}
 
 module.exports = fizzBuzzArray;
