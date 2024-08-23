cf0cc103e9ac68ef817b6c1c9d4ff941a92efc88
Jeffery Abbott <jeff@jefferyabbott.com>
2024-08-23 17:59:12 -0400

    Add telephone number formatting

--- 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
+++ 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
@@ -1,3 +1,9 @@
-function formatPhoneNumber() {}
+function formatPhoneNumber(phoneNumber) {
+    const areaCode = phoneNumber.slice(0,3).join('');
+    const prefix = phoneNumber.slice(3,6).join('');
+    const line = phoneNumber.slice(6).join('');
+
+    return `(${areaCode}) ${prefix}-${line}`;
+}
 
 module.exports = formatPhoneNumber;
