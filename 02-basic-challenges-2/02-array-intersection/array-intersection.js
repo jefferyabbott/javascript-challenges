c97916528e46d084b7e7407b43b3e74cdf3a6b5b
Jeffery Abbott <jeff@jefferyabbott.com>
2024-08-23 07:52:30 -0400

    Add array intersection

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,11 @@
-function arrayIntersection() {}
+function arrayIntersection(arr1, arr2) {
+    const intersection = [];
+    arr1.forEach((item) => {
+        if (arr2.includes(item)) {
+            intersection.push(item);
+        }
+    });
+    return intersection;
+}
 
 module.exports = arrayIntersection;
