4bd1292eaa3ff55720caff56b0b59ce68c58f9fb
Jeffery Abbott <jeff@jefferyabbott.com>
2024-08-23 11:04:43 -0400

    Add find missing number

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,21 @@
-function findMissingNumber() {}
+function findMissingNumber(arr) {
+    if (!arr) {
+        return undefined;
+    }
+    if (arr.length === 0) {
+        return 1;
+    }
+    arr.sort((a, b) => a - b);
+
+    currentNumber = arr[0];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] <= currentNumber + 1) {
+            currentNumber = arr[i];
+        } else {
+            return currentNumber + 1;
+        }
+    }
+
+}
 
 module.exports = findMissingNumber;
