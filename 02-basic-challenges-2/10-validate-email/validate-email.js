589e47949b115b48b7c551a2a54d3cce22f58806
Jeffery Abbott <jeff@jefferyabbott.com>
2024-08-23 18:05:25 -0400

    Add email validation

--- 02-basic-challenges-2/10-validate-email/validate-email.js
+++ 02-basic-challenges-2/10-validate-email/validate-email.js
@@ -1,3 +1,13 @@
-function validateEmail() {}
+function validateEmail(email) {
+    if (!email.includes('@')) {
+        return false;
+    }
+    const splitEmail = email.split('@');
+    if (splitEmail[0].length === 0) {
+        return false;
+    }
+    return splitEmail[1].includes('.');
+
+}
 
 module.exports = validateEmail;
