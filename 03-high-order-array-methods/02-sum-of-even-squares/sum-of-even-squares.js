6d83f33466463af79fbb3ca06fd322760083e485
Jeffery Abbott <jeff@jefferyabbott.com>
2024-08-25 04:46:41 -0400

    Add sum of even squares

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,5 @@
-function sumOfEvenSquares() {}
+function sumOfEvenSquares(arr) {
+    return arr.filter((n) => n % 2 === 0).map((n) => n * n).reduce((total, n) => total + n, 0);
+}
 
 module.exports = sumOfEvenSquares;
