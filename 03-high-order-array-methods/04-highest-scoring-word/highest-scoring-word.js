8fe359cbdb01c39845022442aba20114264749c8
Jeffery Abbott <jeff@jefferyabbott.com>
2024-08-25 16:23:32 -0400

    Add highest scoring word

--- 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
+++ 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
@@ -1,3 +1,16 @@
-function highestScoringWord() {}
+function highestScoringWord(stringOfWords) {
+    let highestScore = 0;
+    let highestScoringWord = '';
+    const words = stringOfWords.split(' ');
+    words.forEach((word) => {
+        const letters = Array.from(word);
+        const sum = letters.reduce((total, letter) => total + letter.charCodeAt(0) - 96, 0);
+        if (sum > highestScore) {
+            highestScore = sum;
+            highestScoringWord = word;
+        }
+    });
+    return highestScoringWord;
+}
 
 module.exports = highestScoringWord;
