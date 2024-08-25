b84257ae58ff1e487630b4fa8072ddbf1551552b
Jeffery Abbott <jeff@jefferyabbott.com>
2024-08-25 16:02:19 -0400

    Add Calculate total sales

--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,3 +1,6 @@
-function calculateTotalSalesWithTax() {}
+function calculateTotalSalesWithTax(products, taxRate) {
+    const sales = products.reduce((total, product) => total + product.price * product.quantity, 0);
+    return sales + sales * taxRate / 100;
+}
 
 module.exports = calculateTotalSalesWithTax;
