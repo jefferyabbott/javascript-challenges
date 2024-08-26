35880ce0880e65de2209dfb475d86b885a723eef
Jeffery Abbott <jeff@jefferyabbott.com>
2024-08-26 07:54:02 -0400

    Add valid anagrams

--- 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
+++ 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
@@ -1,3 +1,17 @@
-function validAnagrams() {}
+function validAnagrams(str1, str2) {
+    if (str1.length !== str2.length) {
+        return false;
+    }
+    const arr1 = Array.from(str1).sort();
+    const arr2 = Array.from(str2).sort();
+    let matched = true;
+    arr1.forEach((item, index) => {
+        if (item !== arr2[index]) {
+            matched = false;
+        } 
+    });
+    return matched;
+}
 
 module.exports = validAnagrams;
+
