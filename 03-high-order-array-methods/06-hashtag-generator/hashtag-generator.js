64f28b6f8eb452514d875c1b3bdb81b0d04a8821
Jeffery Abbott <jeff@jefferyabbott.com>
2024-08-27 04:44:56 -0400

    Add hashtag generator

--- 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
+++ 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
@@ -1,3 +1,16 @@
-function generateHashtag() {}
+function generateHashtag(str) {
+
+    if (str.trim() === '') {
+        return false;
+    }
+
+    let words = str.trim().split(' ');
+    words = words.map((w) => w.charAt(0).toUpperCase() + w.substring(1));
+    const hashtag = words.reduce((ht, word) => ht + word, '#');
+    if (hashtag.length > 140) {
+        return false;
+    }
+    return hashtag;
+}
 
 module.exports = generateHashtag;
