f1183c688f9b7990012975e0508855e3e5ad7dad
Jeffery Abbott <jeff@jefferyabbott.com>
2024-08-28 04:42:25 -0400

    Add valid ipv4 check

--- 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
+++ 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
@@ -1,3 +1,12 @@
-const isValidIPv4 = () => {};
+const isValidIPv4 = (address) => {
+    const octets = address.split('.');
+    if (octets.length !== 4) {
+        return false;
+    }
+    return octets.every((o) => {
+        const octet = parseInt(o);
+        return o >= 0 && o <= 255;
+    });
+};
 
 module.exports = isValidIPv4;
