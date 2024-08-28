012e71b0d2c0ee03ae56de70c309e136c0c935ea
Jeffery Abbott <jeff@jefferyabbott.com>
2024-08-28 14:18:38 -0400

    Add Analyze car mileage

--- 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
+++ 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
@@ -1,3 +1,15 @@
-function analyzeCarMileage() {}
+function analyzeCarMileage(cars) {
+    const totalMileage = cars.reduce((totalMileage, car) => totalMileage + car.mileage, 0);
+    const highestMileageCar = cars.reduce((highestMileage, car) => highestMileage.mileage > car.mileage ? highestMileage : car);
+    const lowestMileageCar = cars.reduce((lowestMileage, car) => lowestMileage.mileage < car.mileage ? lowestMileage : car);
+
+    return {
+        averageMileage: totalMileage / cars.length,
+        highestMileageCar,
+        lowestMileageCar,
+        totalMileage,
+    }
+
+}
 
 module.exports = analyzeCarMileage;
