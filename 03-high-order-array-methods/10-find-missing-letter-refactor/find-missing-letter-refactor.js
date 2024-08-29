c401985fec987597ed8bce163fdcb7b6b58e5c74
Jeffery Abbott <jeff@jefferyabbott.com>
2024-08-29 10:08:17 -0400

    Ad find missing letter refactored

--- 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
+++ 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
@@ -1,3 +1,14 @@
-function findMissingLetter() {}
+function findMissingLetter(letters) {
+    const missingLetter = letters.filter((letter, index) => {
+        if (index === 0) {
+            return false;
+        }
+        const lastLetter = letters[index - 1].charCodeAt(0);
+        const currentLetter = letter.charCodeAt(0);
+        return currentLetter - lastLetter > 1;
+    })[0];
+
+    return missingLetter ? String.fromCharCode(missingLetter.charCodeAt(0) - 1) : '';
+}
 
 module.exports = findMissingLetter;
