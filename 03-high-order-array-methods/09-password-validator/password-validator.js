60769105bf0f7e7a1b958597bc8882e97434dc5b
Jeffery Abbott <jeff@jefferyabbott.com>
2024-08-29 09:29:31 -0400

    add password validator

--- 03-high-order-array-methods/09-password-validator/password-validator.js
+++ 03-high-order-array-methods/09-password-validator/password-validator.js
@@ -1,3 +1,17 @@
-function validatePassword() {}
+function validatePassword(password) {
+    if (password.length < 8) {
+        return false;
+    }
+    if (!/[A-Z]/.test(password)) {
+        return false;
+    } 
+    if (!/[a-z]/.test(password)) {
+        return false;
+    } 
+    if (!/[0-9]/.test(password)) {
+        return false;
+    }
+    return true;
+}
 
 module.exports = validatePassword;
