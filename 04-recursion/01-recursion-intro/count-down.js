080195a26cc03a2898602e6b7b64ef56bbfc5659
Jeffery Abbott <jeff@jefferyabbott.com>
2024-08-30 05:38:34 -0400

    Add simple recursive method

--- 04-recursion/01-recursion-intro/count-down.js
+++ 04-recursion/01-recursion-intro/count-down.js
@@ -1,3 +1,11 @@
-function countDown() {}
+function countDown(n) {
+    if (n <= 0) {
+        console.log('All done!');
+        return;
+    }
+
+    console.log(n);
+    countDown(--n);
+}
 
 module.exports = countDown;
