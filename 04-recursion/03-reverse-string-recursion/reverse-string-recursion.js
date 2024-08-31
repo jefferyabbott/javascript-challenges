b5b4f33b070125dc9267b65590e6a9104359bb41
Jeffery Abbott <jeff@jefferyabbott.com>
2024-08-31 05:16:46 -0400

    basic recursion exercises

--- 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
+++ 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
@@ -1,12 +1,3 @@
-// function reverseString(str) {
-//     // base case
-//     if (str === '') {
-//         return '';
-//     }
-
-//     return reverseString(str.substr(1)) + str.charAt(0);
-// }
-
 const reverseString = (str) => str === '' ? '' : reverseString(str.substr(1)) + str.charAt(0);
 
 module.exports = reverseString;
