652e05041ad8d18c4af8b974f126409cdb28af18
Jeffery Abbott <jeff@jefferyabbott.com>
2024-09-02 08:58:27 -0400

    add number range

--- 04-recursion/08-number-range/number-range.js
+++ 04-recursion/08-number-range/number-range.js
@@ -1,3 +1,11 @@
-function numberRange() {}
+function numberRange(start, end) {
+    if (start === end) {
+        return [start];
+    }
+
+    const numberArr = numberRange(start, end - 1);
+    numberArr.push(end);
+    return numberArr;
+}
 
 module.exports = numberRange;
