8770d6a217978f64c3158faea2d847496b6f0ebb
Jeffery Abbott <jeff@jefferyabbott.com>
2024-09-03 05:01:19 -0400

    Add flatten array

--- 04-recursion/09-flatten-array/flatten-array.js
+++ 04-recursion/09-flatten-array/flatten-array.js
@@ -1,3 +1,18 @@
-function flattenArray() {}
+function flattenArray(arr) {
+    if (typeof(arr) !== Array) {
+        let flatArray = [];
+
+        for (const element of arr) {
+            if (Array.isArray(element)) {
+                flatArray = flatArray.concat(flattenArray(element));
+            } else {
+                flatArray.push(element);
+            }
+        }
+        return flatArray;
+    }
+
+    
+}
 
 module.exports = flattenArray;
