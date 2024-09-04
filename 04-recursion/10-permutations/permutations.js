2abb50514ac9d912cc1d2d02c59b095a6de571c2
Jeffery Abbott <jeff@jefferyabbott.com>
2024-09-04 04:10:35 -0400

    Add permutations

--- 04-recursion/10-permutations/permutations.js
+++ 04-recursion/10-permutations/permutations.js
@@ -1,3 +1,20 @@
-function permutations() {}
+function permutations(str) {
+    const possibleCombinations = [];
+
+    if (str.length === 0) {
+        possibleCombinations.push('');
+        return possibleCombinations;
+    }
+
+    for (let i = 0; i < str.length; i++) {
+        const start = str[i];
+        const letters = str.slice(0, i) + str.slice(i + 1);
+        const remainder = permutations(letters);
+
+        remainder.forEach((r) => possibleCombinations.push(start + r));
+    }
+
+    return possibleCombinations;
+}
 
 module.exports = permutations;
