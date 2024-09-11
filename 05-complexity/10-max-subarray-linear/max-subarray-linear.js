0269d1a0c68a94733adb2f175bcbb4584902b309
Jeffery Abbott <jeff@jefferyabbott.com>
2024-09-11 08:15:26 -0400

    Fix error in test

--- 05-complexity/10-max-subarray-linear/max-subarray-linear.js
+++ 05-complexity/10-max-subarray-linear/max-subarray-linear.js
@@ -1,12 +1,11 @@
 function maxSubarraySum(arr, k) {
-  let currentSum = 0;
   let maxSum = 0;
 
   for (let i = 0; i < k; i++) {
     maxSum += arr[i];
   }
 
-  currentSum = maxSum;
+  let currentSum = maxSum;
 
   for (let i = k; i < arr.length; i++) {
     // subract the first element from the previous window
