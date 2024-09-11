10274f658fc7beebe3f46315d8f3489158f48c11
Jeffery Abbott <jeff@jefferyabbott.com>
2024-09-11 05:26:26 -0400

    Add max subarray linear function

--- 05-complexity/10-max-subarray-linear/max-subarray-linear.js
+++ 05-complexity/10-max-subarray-linear/max-subarray-linear.js
@@ -1,3 +1,21 @@
-function maxSubarraySum() {}
+function maxSubarraySum(arr, k) {
+  let currentSum = 0;
+  let maxSum = 0;
+
+  for (let i = 0; i < k; i++) {
+    maxSum += arr[i];
+  }
+
+  currentSum = maxSum;
+
+  for (let i = k; i < arr.length; i++) {
+    // subract the first element from the previous window
+    // and add the next element
+    currentSum = currentSum - arr[i - k] + arr[i];
+    maxSum = Math.max(maxSum, currentSum);
+  }
+
+  return maxSum;
+}
 
 module.exports = maxSubarraySum;
