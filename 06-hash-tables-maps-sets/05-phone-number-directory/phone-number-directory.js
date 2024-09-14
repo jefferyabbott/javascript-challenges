2ab766e2df5235afddedf4d0483b4e0bc59a0743
Jeffery Abbott <jeff@jefferyabbott.com>
2024-09-14 04:14:34 -0400

    Add phone directory

--- 06-hash-tables-maps-sets/05-phone-number-directory/phone-number-directory.js
+++ 06-hash-tables-maps-sets/05-phone-number-directory/phone-number-directory.js
@@ -1,3 +1,10 @@
-function phoneNumberDirectory() {}
+function phoneNumberDirectory(phoneArr) {
+  const phoneDirectory = new Map();
+  phoneArr.forEach((record) => {
+    const entry = record.split(':');
+    phoneDirectory.set(entry[0], entry[1]);
+  });
+  return phoneDirectory;
+}
 
 module.exports = phoneNumberDirectory;
