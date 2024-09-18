85f13129dbe981ae865ce108b69a863fbe802753
Jeffery Abbott <jeff@jefferyabbott.com>
2024-09-18 10:39:05 -0400

    Create hash function

--- 06-hash-tables-maps-sets/11-custom-hash-table/custom-hash-table-run.js
+++ 06-hash-tables-maps-sets/11-custom-hash-table/custom-hash-table-run.js
@@ -1,1 +1,8 @@
 const HashTable = require('./custom-hash-table');
+const myHashTable = new HashTable();
+
+myHashTable.set('John', '555-555-2341');
+myHashTable.set('James', '814-382-4534');
+myHashTable.set('Jeff', '324-423-1234');
+
+myHashTable.printTable();
