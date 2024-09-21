c52ceb6d6f2253db394930d6ba3c47f1c97ec678
Jeffery Abbott <jeff@jefferyabbott.com>
2024-09-21 05:59:25 -0400

    Add balanced parenthesis function

--- 07-stacks-queues-linked-lists/04-balanced-parenthesis/balanced-parenthesis-run.js
+++ 07-stacks-queues-linked-lists/04-balanced-parenthesis/balanced-parenthesis-run.js
@@ -2,5 +2,8 @@ const balancedParenthesis = require('./balanced-parenthesis');
 
 const result1 = balancedParenthesis('()()()');
 const result2 = balancedParenthesis('()(');
+const result3 = balancedParenthesis('((');
+const result4 = balancedParenthesis('))');
 
-console.log(result1, result2);
+console.log(result1, result2, result3, result4);
+
