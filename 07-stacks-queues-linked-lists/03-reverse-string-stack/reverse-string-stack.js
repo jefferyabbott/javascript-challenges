2257a350b7e974b0e2045e2643bc1b64d3a14035
Jeffery Abbott <jeff@jefferyabbott.com>
2024-09-21 05:38:34 -0400

    Add reversed string using stack

--- 07-stacks-queues-linked-lists/03-reverse-string-stack/reverse-string-stack.js
+++ 07-stacks-queues-linked-lists/03-reverse-string-stack/reverse-string-stack.js
@@ -1,5 +1,13 @@
 const Stack = require('./stack');
 
-function reverseStringStack() {}
+function reverseStringStack(str) {
+  const stack = new Stack();
+  str.split('').forEach((char) => stack.push(char));
+  let reversed = '';
+  while (!stack.isEmpty()) {
+    reversed += stack.pop();
+  }
+  return reversed;
+}
 
 module.exports = reverseStringStack;
