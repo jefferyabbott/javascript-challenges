1c978d05d352b6aa834ea5d4bbf05f4e7a636656
Jeffery Abbott <jeff@jefferyabbott.com>
2024-09-22 03:53:48 -0400

    Add reversed string using queue

--- 07-stacks-queues-linked-lists/07-reverse-string-queue/reverse-string-queue.js
+++ 07-stacks-queues-linked-lists/07-reverse-string-queue/reverse-string-queue.js
@@ -1,5 +1,15 @@
 const Queue = require('./queue');
 
-const reverseStringWithQueue = (str) => {};
+const reverseStringWithQueue = (str) => {
+  const q = new Queue();
+  for (let i = str.length - 1; i >= 0; i--) {
+    q.enqueue(str[i]);
+  }
+  let reversedStr = '';
+  while (!q.isEmpty()) {
+    reversedStr += q.dequeue();
+  }
+  return reversedStr;
+};
 
 module.exports = reverseStringWithQueue;
