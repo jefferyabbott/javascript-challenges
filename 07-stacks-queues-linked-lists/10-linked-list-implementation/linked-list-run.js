5c6db7f0ec7f34acc8b840ef6d3951f71d166382
Jeffery Abbott <jeff@jefferyabbott.com>
2024-09-24 04:08:53 -0400

    Add linked list implementation

--- 07-stacks-queues-linked-lists/10-linked-list-implementation/linked-list-run.js
+++ 07-stacks-queues-linked-lists/10-linked-list-implementation/linked-list-run.js
@@ -1,1 +1,26 @@
 const { LinkedList } = require('./linked-list');
+
+const list = new LinkedList();
+list.add(100);
+list.add(200);
+list.add(300);
+list.add(400);
+
+// list.printAll();
+// console.log(list.get(2));
+// console.log(list.get(3));
+// console.log(list.get(4));
+// console.log(list.get(7));
+
+list.insertAt(1, 'a');
+// list.printAll();
+
+
+list.insertAt(12, 'b');
+// list.printAll();
+
+console.log(list.removeFrom(1));
+// list.printAll();
+
+console.log(list.removeFrom(100));
+list.printAll();
--- 07-stacks-queues-linked-lists/10-linked-list-implementation/linked-list.js
+++ 07-stacks-queues-linked-lists/10-linked-list-implementation/linked-list.js
@@ -1,5 +1,95 @@
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
 
-class LinkedList {}
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  add(data) {
+    const node = new Node(data);
+    if (this.head === null) {
+      this.head = node;
+    } else {
+      this.tail.next = node;
+    }
+    this.tail = node;
+  }
+
+  insertAt(index, data) {
+    const node = new Node(data);
+    if (index === 0) {
+      node.next = this.head;
+      this.head = node;
+    } else {
+      let current = this.head;
+      let previous = null;
+      let i = 0;
+
+      while (i < index) {
+        if (current.next === null) {
+          return false;
+        }
+        previous = current;
+        current = current.next;
+        i++;
+      }
+
+      node.next = current;
+      previous.next = node;
+      return true;
+    }
+  }
+
+  removeFrom(index) {
+    if (index === 0) {
+      const value = this.head.data;
+      this.head = this.head.next;
+      return value;
+    } else {
+      let current = this.head;
+      let previous = null;
+      let i = 0;
+
+      while (i < index) {
+        if (current.next === null) {
+          return undefined;
+        }
+        previous = current;
+        current = current.next;
+        i++;
+      }
+      const value = current.data;
+      previous.next = current.next;
+      return value;
+    }
+  }
+
+  get(index) {
+    let current = this.head;
+    let i = 0;
+    while (i < index) {
+      if (current.next === null) {
+        return undefined;
+      }
+      current = current.next;
+      i++;
+    }
+    return current.data;
+  }
+
+  printAll() {
+    let current = this.head;
+    while (current !== null) {
+      console.log(current.data);
+      current = current.next;
+    }
+  }
+}
 
 module.exports = { Node, LinkedList };
