3ff72a6ec111526685ec451d442bb07111c5a5c5
Jeffery Abbott <jeff@jefferyabbott.com>
2024-09-24 04:14:43 -0400

    Add reversed string using linked list

--- 07-stacks-queues-linked-lists/11-reverse-string-linked-list/reverse-string-linked-list.js
+++ 07-stacks-queues-linked-lists/11-reverse-string-linked-list/reverse-string-linked-list.js
@@ -1,5 +1,17 @@
 const { LinkedList } = require('./linked-list');
 
-function reverseStringLinkedList() {}
+function reverseStringLinkedList(str) {
+  const list = new LinkedList();
+  for (let i = str.length - 1; i >= 0; i--) {
+    list.add(str[i]);
+  }
+  let reversedStr = '';
+  let current = list.head;
+  while (current !== null) {
+    reversedStr += current.data;
+    current = current.next;
+  }
+  return reversedStr;
+}
 
 module.exports = reverseStringLinkedList;
