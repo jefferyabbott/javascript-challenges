43a3212df8210342caf803a55a2eea239eea0a53
Jeffery Abbott <jeff@jefferyabbott.com>
2024-09-26 08:02:54 -0400

    Add doubly linked list

--- 07-stacks-queues-linked-lists/15-doubly-linked-list-implementation/doubly-linked-list-run.js
+++ 07-stacks-queues-linked-lists/15-doubly-linked-list-implementation/doubly-linked-list-run.js
@@ -1,2 +1,17 @@
 const DoublyLinkedList = require('./doubly-linked-list');
 
+const list = new DoublyLinkedList();
+list.append(100);
+list.append(200);
+list.append(300);
+list.append(400);
+
+// list.printAll();
+
+list.prepend(50);
+
+// list.printAll();
+
+list.insertAt(2, 'new');
+
+list.printAll();
