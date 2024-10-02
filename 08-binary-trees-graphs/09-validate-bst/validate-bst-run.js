f68546f0ede0d097f7bc932f6169b6b240cfb4c1
Jeffery Abbott <jeff@jefferyabbott.com>
2024-10-02 06:00:01 -0400

    Add validate binary search tree

--- 08-binary-trees-graphs/09-validate-bst/validate-bst-run.js
+++ 08-binary-trees-graphs/09-validate-bst/validate-bst-run.js
@@ -7,3 +7,19 @@ const { Node, isValidBST } = require('./validate-bst');
 //    / \
 //   2   6
 
+const root = new Node(8);
+const node4 = new Node(4);
+const node10 = new Node(10);
+const node2 = new Node(2);
+const node6 = new Node(6);
+const node100 = new Node(100);
+
+
+root.left = node4;
+root.right = node10;
+node4.left = node2;
+node4.right = node6;
+node4.right = node100;
+
+const result = isValidBST(root);
+console.log(result);
