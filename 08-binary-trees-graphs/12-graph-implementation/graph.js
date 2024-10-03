dbd3e7f63759f1963e76096c606c7c28005940e9
Jeffery Abbott <jeff@jefferyabbott.com>
2024-10-03 07:01:41 -0400

    Add graph adjacency list implementation

--- 08-binary-trees-graphs/12-graph-implementation/graph.js
+++ 08-binary-trees-graphs/12-graph-implementation/graph.js
@@ -1,4 +1,36 @@
-class Graph {}
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addVertex(vertex) {
+    this.adjacencyList[vertex] = [];
+  }
+
+  addEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1].push(vertex2);
+    this.adjacencyList[vertex2].push(vertex1);
+  }
+
+  removeVertex(vertex) {
+    while (this.adjacencyList[vertex].length) { 
+      const adjacencyVertex = this.adjacencyList[vertex].pop();
+      this.removeEdge(vertex, adjacencyVertex);
+    }
+    delete this.adjacencyList[vertex];
+  }
+
+  removeEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter((v) => v !== vertex2);
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter((v) => v !== vertex1);
+  }
+
+  printAdjacencyList() {
+    for (const vertex in this.adjacencyList) {
+      console.log(`${vertex} -> ${this.adjacencyList[vertex].join('')}`);
+    }
+  }
+}
 
 // Example Adjacency List
 // {
