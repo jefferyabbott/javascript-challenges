33df313d09b961244aeed57926625c37387b7687
Jeffery Abbott <jeff@jefferyabbott.com>
2024-10-08 05:23:18 -0400

    add insertion sort

--- 09-sorting-algorithms/05-insertion-sort-implementation/insertion-sort.js
+++ 09-sorting-algorithms/05-insertion-sort-implementation/insertion-sort.js
@@ -1,3 +1,14 @@
-function insertionSort() {}
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    const currentElement = arr[i];
+    let lastSortedElementIndex = i - 1;
+    while (lastSortedElementIndex >= 0 && arr[lastSortedElementIndex] > currentElement) {
+      arr[lastSortedElementIndex + 1] = arr[lastSortedElementIndex];
+      lastSortedElementIndex--;
+    }
+    arr[lastSortedElementIndex + 1] = currentElement;
+  }
+  return arr;
+}
 
 module.exports = insertionSort;
