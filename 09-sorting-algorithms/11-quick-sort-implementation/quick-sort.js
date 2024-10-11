ea14f0c732888801b60116f817f0c651f47e14eb
Jeffery Abbott <jeff@jefferyabbott.com>
2024-10-11 08:05:04 -0400

    Add quick sort implementation

--- 09-sorting-algorithms/11-quick-sort-implementation/quick-sort.js
+++ 09-sorting-algorithms/11-quick-sort-implementation/quick-sort.js
@@ -1,5 +1,22 @@
-function quickSort() {
+function quickSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
 
+  const pivot = arr[arr.length - 1];
+
+  const lessThanPivot = [];
+  const greaterThanOrEqualPivot = [];
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] < pivot) {
+      lessThanPivot.push(arr[i]);
+    } else {
+      greaterThanOrEqualPivot.push(arr[i]);
+    }
+  }
+
+  return [...quickSort(lessThanPivot), pivot, ...quickSort(greaterThanOrEqualPivot)];
 }
 
 module.exports = quickSort;
